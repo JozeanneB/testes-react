3d467bdd8e7228e0da150c2f101ff860df29d5e5
Jozeanne <jozeanne.belomo@gmail.com>
2017-02-20 16:08:19 -0300

    Eat all apples Button

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -7,6 +7,8 @@ class HelloWorld extends Component {
   constructor(props) {
     super(props);
     this.state = { date: new Date(), number: 2 };
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -31,13 +33,20 @@ class HelloWorld extends Component {
     });
   }
 
-
+  handleClick() {
+    this.setState({
+      number: 0
+    });
+  }
 
   render() {
     return (
         <div>
           <h1>HelloWorld</h1>
           <h2>It is {this.state.date.toLocaleTimeString()}. And I have {this.state.number} apples.</h2>
+          <button onClick={this.handleClick}>
+            <b> Eat all apples </b>
+          </button>
         </div>
 
     );
